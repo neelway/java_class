253bbf71d5264c914f40e5e691996ec590389d6e
Neelway <neeleshtriapthi786@gmail.com>
2018-03-06 11:53:18 +0530

    split program

---
+++ Class9_split_neelesh.java
@@ -0,0 +1,17 @@
+
+public class Class9_split_neelesh {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			String s1="java program in the split";  
+			String[] words=s1.split(" "); 
+			for(String w:words){  
+			System.out.println(w);  
+			}
+	}
+
+}
